1055d5def69eda83e8e2842caf5f0aaf1c6c8702
Alex <ajryan@wpi.edu>
2013-01-07 03:02:51 -0600

    Added Stuff

--- Team2503FRC2013/src/edu/wpi/first/wpilibj/templates/Team2503RobotMain.java
+++ Team2503FRC2013/src/edu/wpi/first/wpilibj/templates/Team2503RobotMain.java
@@ -8,9 +8,10 @@
 package edu.wpi.first.wpilibj.templates;
 
 
-import edu.wpi.first.wpilibj.SimpleRobot;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,14 +22,44 @@ import edu.wpi.first.wpilibj.Jaguar;
  */
 public class Team2503RobotMain extends SimpleRobot {
     
+    //CONSTANTS BEGIN HERE
+    
+    static final int JoystickNumber = 1; //This is the joystick # the driverstation must set the gamepad to
+    
+    static final int LeftJag1PWMSlot = 1;
+    static final int LeftJag2PWMSlot = 2;
+    static final int RightJag1PWMSlot = 3;
+    static final int RightJag2PWMSlot = 4;
+    
     static final int ForwardAxis = 1;
     static final int TurnAxis = 4;
     
-    Joystick GamePad;
+    //CONSTANTS END HERE
+    
+    //ROBOT SYSTEMS AND VARIABLES START HERE
+    
+    Joystick gamePad;
+    Jaguar leftJag1, leftJag2, rightJag1, rightJag2;
+    
+    
+    //ROBOT SYSTEMS AND VARIABLES END HERE
+    
+    
     
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
+    
+    Team2503RobotMain(){
+        gamePad = new Joystick(JoystickNumber);
+        
+        leftJag1 = new Jaguar(LeftJag1PWMSlot);
+        leftJag2 = new Jaguar(LeftJag2PWMSlot);
+        rightJag1 = new Jaguar(RightJag1PWMSlot);
+        rightJag2 = new Jaguar(RightJag2PWMSlot);
+        
+    }
+    
     public void autonomous() {
         
     }
@@ -39,7 +70,8 @@ public class Team2503RobotMain extends SimpleRobot {
     public void operatorControl() {
         while(isOperatorControl()){
             
-            JoyPadDrive(GamePad.getRawAxis(ForwardAxis), GamePad.getRawAxis(TurnAxis));
+            TypicalJoyPadDrive(); //Right now, just drive around using arcade drive for arbitraty sticks.
+            Timer.delay(0.05);
             
         }
     }
@@ -47,8 +79,10 @@ public class Team2503RobotMain extends SimpleRobot {
     private void JoyPadDrive(double forward, double turn){
 
 
-
-
+    }
+    
+    private void TypicalJoyPadDrive(){
+        JoyPadDrive(gamePad.getRawAxis(ForwardAxis), gamePad.getRawAxis(TurnAxis));
     }
 }
 
