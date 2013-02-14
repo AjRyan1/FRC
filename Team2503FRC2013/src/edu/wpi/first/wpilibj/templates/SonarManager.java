8b4aa8f7c39e7ecaedf2324ad347f15de15754c1
Alex <ajryan@wpi.edu>
2013-02-13 23:14:03 -0500

    Changes

---
+++ Team2503FRC2013/src/edu/wpi/first/wpilibj/templates/SonarManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * This class is designed to enable automatic management of MaxBotix sensors through the use of software timers that 
+ * should automatically enable and disable sonar sensors to prevent interference - potentially not as useful on newer 
+ * HRLV line which can just be left on, but the older XL and LV sensors would greatly benefit from the management
+ * @author Alex
+ * 
+ * 
+ * WIP
+ */
+public class SonarManager {
+    //Class Variables - WIP
+    
+    SonarManager(){
+        
+    }
+    
+}
