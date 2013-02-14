9426fd9db9023895aa36798527cefefa3204d3e8
Alex <ajryan@wpi.edu>
2013-02-13 23:14:14 -0500

    Changes

---
+++ Team2503FRC2013/src/edu/wpi/first/wpilibj/templates/MaxBotixHRLV.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Alex
+ */
+public class MaxBotixHRLV implements IUltrasonicSensor {
+    public double returnDistanceFeet(){
+        return 0.0;
+    }
+    public double returnDistanceInches(){
+        return 0.0;
+    }
+    public double returnDistanceCentimeters(){
+        return 0.0;
+    }
+    public double returnDistanceMillimeters(){
+        return 0.0;
+    }
+    
+    public void enableSampling(){ 
+        return;
+    }
+    public void disableSampling(){  
+        return;
+    }
+    public double millisSinceLastReading(){ 
+        return 0.0;
+    }
+}
